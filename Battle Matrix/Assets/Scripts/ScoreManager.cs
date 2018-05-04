72db6d4acb9249a045f3b87a47b061845da85325
SeanWiz <SeanWiz@users.noreply.github.com>
2018-05-04 19:13:34 -0400

    Score Manager Framework

---
+++ Battle Matrix/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+    int currentRound;
+    public int totalRounds;
+
+    int p1RoundsWon = 0;
+    int p2RoundsWon = 0;
+
+	// Use this for initialization
+	void Start () {
+        currentRound = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void NewRound()
+    {
+        if (currentRound < totalRounds)
+            currentRound += 1;
+        else
+            EndGame();
+    }
+
+    void EndGame()
+    {
+        return;
+    }
+}
