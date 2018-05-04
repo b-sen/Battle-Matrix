4091fc8f4c94179e6293b18e75eadaa97e50515f
SeanWiz <SeanWiz@users.noreply.github.com>
2018-05-04 17:58:59 -0400

    Something about icons activating

--- Battle Matrix/Assets/Scripts/PlayerManager.cs
+++ Battle Matrix/Assets/Scripts/PlayerManager.cs
@@ -7,15 +7,18 @@ public class PlayerManager : MonoBehaviour {
     // Exists to contain the names of each player's input axis
     public struct PlayerControls
     {
-        string horizontal;
-        string rotate;
-        string drop;
+        public string horizontal;
+        public string rotate;
+        public string drop;
 
-        public PlayerControls(string hz, string rt, string dr)
+        public List<GameObject> icons;
+
+        public PlayerControls(string hz, string rt, string dr, List<GameObject> ic)
         {
             this.horizontal = hz;
             this.rotate = rt;
             this.drop = dr;
+            this.icons = ic;
         }
     }
 
@@ -28,12 +31,67 @@ public class PlayerManager : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        p1 = new PlayerControls("P1Horizontal", "P1Rotate", "P1Drop");
-        p2 = new PlayerControls("P2Horizontal", "P2Rotate", "P2Drop");
+        p1 = new PlayerControls("P1Horizontal", "P1Rotate", "P1Drop", p1Icons);
+        p2 = new PlayerControls("P2Horizontal", "P2Rotate", "P2Drop", p2Icons);
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        CheckInputs(p1);
+        CheckInputs(p2);
+    }
+
+    void CheckInputs(PlayerControls player)
+    {
+        float hz = Input.GetAxis(player.horizontal);
+        float rt = Input.GetAxis(player.rotate);
+        float dr = Input.GetAxis(player.drop);
+
+        // Check Horizontal
+        if (hz < 0)
+        {
+            player.icons[0].GetComponent<IconActivation>().Activation();
+            player.icons[1].GetComponent<IconActivation>().Deactivation();
+        }
+        else if (hz > 0)
+        {
+            player.icons[0].GetComponent<IconActivation>().Deactivation();
+            player.icons[1].GetComponent<IconActivation>().Activation();
+        }
+        else
+        {
+            player.icons[0].GetComponent<IconActivation>().Deactivation();
+            player.icons[1].GetComponent<IconActivation>().Deactivation();
+        }
+
+        // Check Rotation
+        if (rt < 0)
+        {
+            player.icons[2].GetComponent<IconActivation>().Activation();
+            player.icons[3].GetComponent<IconActivation>().Deactivation();
+        }
+        else if (rt > 0)
+        {
+            player.icons[2].GetComponent<IconActivation>().Deactivation();
+            player.icons[3].GetComponent<IconActivation>().Activation();
+        }
+        else
+        {
+            player.icons[2].GetComponent<IconActivation>().Deactivation();
+            player.icons[3].GetComponent<IconActivation>().Deactivation();
+        }
+
+        // Check Drop
+        if (dr != 0)
+        {
+            player.icons[4].GetComponent<IconActivation>().Activation();        
+        }
+        else
+        {
+            player.icons[4].GetComponent<IconActivation>().Deactivation();
+        }
+
+
+    }
+
 }
