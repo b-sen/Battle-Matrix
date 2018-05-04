1203af8c5c166486c1e233acfee886e86a7f6057
b-sen <794427+b-sen@users.noreply.github.com>
2018-05-04 17:07:24 -0400

    Control Hooks for Game Logic
    Set up the functions to call for transmitting player input to the game logic as actions.

---
+++ Battle Matrix/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// In charge of all game logic.
+/// </summary>
+public class GameManagerScript : MonoBehaviour {
+    public class PlayerBoard {
+
+
+
+
+
+
+        /// <summary>
+        /// The movement functions, which perform actions that are based directly on player input.  Call these from controls code.
+        /// </summary>
+        public void RotateClockwise() {
+
+        }
+        public void RotateCounterClockwise() {
+
+        }
+        public void SlideLeft() {
+
+        }
+        public void SlideRight() {
+
+        }
+        public void FastDrop() {
+
+        }
+
+    }
+
+    public PlayerBoard player1;
+    public PlayerBoard player2;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    
+    
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
