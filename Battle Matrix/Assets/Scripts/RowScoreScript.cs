d68f1c232e671a99d7cc525ae6211ef38d6bde02
SeanWiz <SeanWiz@users.noreply.github.com>
2018-05-06 13:24:38 -0400

    wrapping up some score stuff

--- Battle Matrix/Assets/Scripts/RowScoreScript.cs
+++ Battle Matrix/Assets/Scripts/RowScoreScript.cs
@@ -8,17 +8,25 @@ public class RowScoreScript : MonoBehaviour {
     public struct PlayerScore
     {
         public int[] attacks;
-        public List<Text> rows;
+        public List<GameObject> rows;
         
         public int roundsWon;
         public int multiplier;
-        public Text roundsText;
-        public Text multiText;
+        public GameObject roundsText;
+        public GameObject multiText;
 
     }
 
 
     public GameManagerScript gm;
+
+    public List<GameObject> rows1;
+    public List<GameObject> rows2;
+
+    public GameObject rounds1;
+    public GameObject rounds2;
+    public GameObject multi1;
+    public GameObject multi2;
 
     GameManagerScript.PlayerBoard[] pb;
 
@@ -29,6 +37,22 @@ public class RowScoreScript : MonoBehaviour {
 
         ps[0] = new PlayerScore();
         ps[1] = new PlayerScore();
+
+        ps[0].rows = rows1;
+        ps[0].roundsText = rounds1;
+        ps[0].multiText = multi1;
+
+        ps[1].rows = rows2;
+        ps[1].roundsText = rounds2;
+        ps[1].multiText = multi2;
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < ps[i].rows.Count; j++)
+            {
+                ps[i].rows[j].GetComponent<Text>().text = "";
+            }
+        }
 
         if (gm)
         {
@@ -50,24 +74,23 @@ public class RowScoreScript : MonoBehaviour {
             pb[1] = gm.player2;
 
 
-
         for(int i = 0; i < 2; i++)
         {
             ps[i].attacks = pb[i].GetAttackTotals();
             ps[i].multiplier = pb[i].GetRoundMultiplier();
             ps[i].roundsWon = gm.GetPlayerRoundsWon(i + 1);
 
-            ps[i].multiText.text = "";
-            ps[i].roundsText.text = "";
+            ps[i].multiText.GetComponent<Text>().text = "Rounds Won: " + ps[i].multiplier.ToString();
+            ps[i].roundsText.GetComponent<Text>().text = "Multiplier: " + ps[i].roundsWon.ToString();
 
             for (int j = 0; j < ps[i].attacks.Length; j++)
             {
                 if(ps[i].rows[j])
                 {
                     if (ps[i].attacks[j] == 0)
-                        ps[i].rows[j].text = "";
+                        ps[i].rows[j].GetComponent<Text>().text = "";
                     else
-                        ps[i].rows[j].text = ps[i].attacks[j].ToString();
+                        ps[i].rows[j].GetComponent<Text>().text = ps[i].attacks[j].ToString();
                 }
                 
             }
